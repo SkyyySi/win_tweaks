b2cf435eac42d972c0ef1b793fff56b8c8a5022b
SkyyySi <rasti.b@protonmail.com>
2021-10-09 12:49:33 +0200

    first commit

---
+++ autohide_tb_off.ps1
@@ -0,0 +1,5 @@
+$path='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+$value=(Get-ItemProperty -Path $path).Settings
+$value[8]=2
+&Set-ItemProperty -Path $path -Name Settings -Value $value
+&Stop-Process -f -ProcessName explorer
